70060f7416bfc73ac73b248d9c52534b7a2e4603
GabryelVenzel <gabryelvenzel@gmail.com>
2025-04-25 07:18:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -104,7 +104,7 @@ with st.expander("🔒 Área restrita: Cadastro e Gerenciamento de Isolantes", e
 
         @st.cache_data
         def carregar_dados():
-            url = f"https://docs.google.com/spreadsheets/d/1W1JHXAnGJeWbGVK0AmORux5I7CYTEwoBIvBfVKO40aY/gviz/tq?tqx=out:csv&sheet=isolantes"
+            url = f"https://docs.google.com/spreadsheets/d/1W1JHXAnGJeWbGVK0AmORux5I7CYTEwoBIvBfVKO40aY/edit#gid=0/gviz/tq?tqx=out:csv&sheet=Isolantes"
 worksheet = sheet.worksheet{"Isolante"}"
             return pd.read_csv(url)
 
