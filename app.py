6771e16e29110a041cdf21f9bd76083bc4a7729d
GabryelVenzel <gabryelvenzel@gmail.com>
2025-04-25 15:07:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -259,24 +259,26 @@ if st.button("Calcular Temperatura da Face Fria"):
     st.session_state.Tf = Tf
 
 # --- RESULTADOS ---
+st.subheader("Resultados")
+
 if st.session_state.convergiu is not None:
     if st.session_state.convergiu:
         st.success(f"\U00002705 Temperatura da face fria: {st.session_state.Tf:.1f} °C".replace('.', ','))
     else:
         st.error("\U0000274C O cálculo não convergiu dentro do limite de iterações.")
 
-    st.subheader("Resultados")
+    
 
     if st.session_state.q_transferencia is not None:
         perda_com = st.session_state.q_transferencia / 1000
-        st.info(f"Perda total com isolante: {str(perda_com).replace('.', ',')[:6]} kW/m²")
+        st.info(f"Perda com isolante: {str(perda_com).replace('.', ',')[:6]} kW/m²")
 
         hr_sem = e * sigma * ((Tq + 273.15)**4 - (To + 273.15)**4)
         h_total_sem = calcular_h_conv(Tq, To, L_total) + hr_sem / (Tq - To)
         q_sem_isolante = h_total_sem * (Tq - To)
 
         perda_sem = q_sem_isolante / 1000
-        st.warning(f"Perda total sem o uso de isolante: {str(perda_sem).replace('.', ',')[:6]} kW/m²")
+        st.warning(f"Perda sem o uso de isolante: {str(perda_sem).replace('.', ',')[:6]} kW/m²")
 
     st.markdown(f"**Espessura total considerada:** {L_total*1000:.1f} mm".replace('.', ','))
 
